a448eb39a52ab061a409a5b3513b2de07b51bcae
galborabia <49067563+galborabia@users.noreply.github.com>
2020-07-11 11:07:53 +0300

    Add files via upload
    start gedalia

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <NavBar /> 
+     <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar"
+export default {
+  name: "App",
+  components: {
+    NavBar
+  },
+  methods: {
+    Logout() {
+      this.$root.store.logout();
+      this.$root.toast("Logout", "User logged out successfully", "success");
+
+      this.$router.push("/").catch(() => {
+        this.$forceUpdate();
+      });
+    }
+  }
+};
+</script>
+
