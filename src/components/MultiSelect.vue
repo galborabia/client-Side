255863a74b8f493334286244a7ae6e45bdcacbb4
galborabia <49067563+galborabia@users.noreply.github.com>
2020-07-11 11:25:56 +0300

    Add files via upload

---
+++ src/components/MultiSelect.vue
@@ -0,0 +1,36 @@
+<template>
+<div> 
+  <div class="m-control-wrapper">
+    <div id="multiselect-sample" class="control-section" style="height: 500px">
+        <ejs-multiselect id='sample-list3' :dataSource='cuisines' :mode='delimiterMode' :placeholder='waterMark'></ejs-multiselect>
+    </div>
+  </div>
+</div>
+</template>
+<style scoped>
+    .m-control-wrapper {
+        margin: 0 auto;
+        width: 64%;
+    }
+</style>
+<script>
+import Vue from "vue";
+import { MultiSelectPlugin } from "@syncfusion/ej2-vue-dropdowns";
+import cuisines from "../assets/intolerances";
+Vue.use(MultiSelectPlugin);
+
+export default Vue.extend ({
+    data: function() {
+        return {
+            waterMark: 'Choose cuisine',
+            defaultMode: 'Default',
+            boxMode: 'Box',
+            delimiterMode: 'Delimiter',
+            cuisines: [{ value: null, text: "", disabled: true }]
+        };
+    },
+   mounted() {
+    this.cuisines.push(...cuisines);
+    },
+});
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item ><router-link :to="{ name: 'Home' }">Home</router-link></b-nav-item>
+        <b-nav-item ><router-link :to="{ name: 'Search' }">Search</router-link></b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto" type="dark">
+        <b-nav-item ><router-link :to="{ name: 'Register' }" right>Register</router-link></b-nav-item>
+        <b-nav-item ><router-link :to="{ name: 'Login' }" right>Login</router-link></b-nav-item>
+        <b-nav-item ><router-link :to="{ name: 'Home' }" right>Logout</router-link></b-nav-item>
+        <b-nav-item-dropdown text="Profile" right>
+        <b-dropdown-item ><router-link :to="{ name: 'Register' }" right>Personal recipes</router-link></b-dropdown-item>
+        <b-dropdown-item ><router-link :to="{ name: 'Register' }" right>Family recipes</router-link></b-dropdown-item>
+        <b-dropdown-item ><router-link :to="{ name: 'Register' }" right>Favorites recipes</router-link></b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
